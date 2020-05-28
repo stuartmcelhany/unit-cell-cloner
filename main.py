af9619abb01a3aa53258d3134c0e79a0fcbfb26b
Stuart McElhany <37053381+stuartmcelhany@users.noreply.github.com>
2020-05-28 12:32:45 -0700

    Updated functionality of main.py
    main.py should be able to take in .xyz files with various delimiters

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 # Stuart McElhany
 import csv
+import math
 
-# triclinic unit cell
-UNIT_DIST_X = [17.53, 0.0, 0.0] # x translation
-UNIT_DIST_Y = [8.765, 15.182, 0.0] # y translation
-UNIT_DIST_Z = [8.765, 5.061, 14.313] # z translation
+# trigonal unit cell dimensions
+SIDE_LENGTH = 17.516
+ANGLE = 60 * (math.pi/180) # first number in degrees
+
+# creates primitive vectors for trigonal cell
+PRIM_VEC_X = [SIDE_LENGTH, 0.0, 0.0] # x translation (x, y, z)
+PRIM_VEC_Y = [SIDE_LENGTH*math.cos(ANGLE), SIDE_LENGTH*math.sin(ANGLE), 0.0] # y translation
+cx = SIDE_LENGTH*math.cos(ANGLE)
+cy = SIDE_LENGTH*(math.cos(ANGLE)-math.cos(ANGLE)**2)/math.sin(ANGLE)
+PRIM_VEC_Z = [cx, cy, math.sqrt(SIDE_LENGTH**2-cx**2-cy**2)] # z translation
 
 results = []
 
+def clean_row_up(row):
+    for value in list(row):
+        if value == '':
+            row.remove(value)
+    return row
+
 print("Enter file name (.xyz)") # file io
 filename = input()
-
-print("Enter cube length")
-n = int(input())
 
 molecule_name = []
 
@@ -22,19 +32,20 @@ with open(filename) as File:
     molecule_name = next(reader) # skips comment line per .xyz convention
     atom_count = 0
     for row in reader:
-        for i in range(n):
-            for j in range(n):
-                for k in range(n):
+        row = clean_row_up(row)
+        for i in range(3):
+            for j in range(3):
+                for k in range(3):
                     row_entry = [""]*4
                     row_entry[0] = row[0] #get atom
-                    row_entry[1] = "{:.4f}".format(float(row[1]) + float(i*UNIT_DIST_X[0]) + float(j*UNIT_DIST_Y[0]) + float(k*UNIT_DIST_Z[0])) # get x
-                    row_entry[2] = "{:.4f}".format(float(row[2]) + float(i*UNIT_DIST_X[1]) + float(j*UNIT_DIST_Y[1]) + float(k*UNIT_DIST_Z[1])) # get y
-                    row_entry[3] = "{:.4f}".format(float(row[3]) + float(i*UNIT_DIST_X[2]) + float(j*UNIT_DIST_Y[2]) + float(k*UNIT_DIST_Z[2])) # get z
+                    row_entry[1] = "{:.4f}".format(float(row[1]) + float((i-1)*PRIM_VEC_X[0]) + float((j-1)*PRIM_VEC_Y[0]) + float((k-1)*PRIM_VEC_Z[0])) # get x
+                    row_entry[2] = "{:.4f}".format(float(row[2]) + float((i-1)*PRIM_VEC_X[1]) + float((j-1)*PRIM_VEC_Y[1]) + float((k-1)*PRIM_VEC_Z[1])) # get y
+                    row_entry[3] = "{:.4f}".format(float(row[3]) + float((i-1)*PRIM_VEC_X[2]) + float((j-1)*PRIM_VEC_Y[2]) + float((k-1)*PRIM_VEC_Z[2])) # get z
                     atom_count += 1
                     results.append(row_entry)
 
 filename = filename.replace('.xyz' , '') # adds .xyz to new file name
-output_file = filename + str(n) + 'x' + str(n) + 'x' + str(n) + '.xyz'
+output_file = filename + str('-3x3x3') + '.xyz'
 with open(output_file, 'w') as File:
     writer = csv.writer(File, delimiter = ' ', lineterminator = '\n')
     writer.writerow([atom_count]) # write atom count line
